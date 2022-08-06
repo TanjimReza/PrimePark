2c9beb735b8eb7e5b3e82d476541c072637b00d4
Tanjim Reza <35164789+TanjimReza@users.noreply.github.com>
2022-08-07 00:33:34 +0600

    Template ready

--- home/views.py
+++ home/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'base-sidebar.html')
+    return render(request, 'home/dashboard.html')
 
 def login(request):
     return render(request, 'home/login.html')
