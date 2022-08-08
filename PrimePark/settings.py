c16db24a737f4eae05ca136763ab27e8293e47dc
Tanjim Reza <35164789+TanjimReza@users.noreply.github.com>
2022-08-09 00:26:33 +0600

    remote database

--- PrimePark/settings.py
+++ PrimePark/settings.py
@@ -81,13 +81,21 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'primepark',
+    #     'USER': 'root',
+    #     'PASSWORD': '',
+    #     'HOST': 'localhost',
+    #     'PORT': '3306',
+    # }
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'primepark',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'NAME': 'tanjimre_primepark',
+        'USER': 'tanjimre_primeparkuser',
+        'PASSWORD': 'cff70978c4053',
+        'HOST': 'server-arizona-vps.quattic.com',
+        # 'PORT': '3306',
     }
     }
 
